bf0278f9b635dff9ad146c6f8b7e2b8cedd086da
jyou <jyou@student.42.kr>
2020-11-15 18:23:53 +0900

    gnl 내용이해 대충 ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jyou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 16:23:35 by jyou              #+#    #+#             */
-/*   Updated: 2020/11/15 17:01:26 by jyou             ###   ########.fr       */
+/*   Updated: 2020/11/15 18:23:23 by jyou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 
 int			get_next_line(int fd, char **line)
 {
+	char	buff[BUFFER_SIZE + 1];
 	int		i;
 	int		len;
 
-	i = 0;
-	len = get_len(fd, line);
+	//i = 0;
+	//len = get_len(fd, line);
+	if (!line || fd < 0 || BUFFER_SIZE < 1 || read(fd, buff, 0) < 0) // err
+		return (-1);
+	// \n 나올때까지 인덱스 찾아서 substr 해서 line[i]에 저장.
+	// 터지면 free
+	// 끝이 eof 면 0
+	// 아니면 1
 	while (line[i])
 	{
 		if (!(line[i] = (char *)malloc(sizeof(char) * (len + 1))))
